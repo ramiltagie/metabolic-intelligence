589b7c826a2e514090d23b10985319955b13c980
ramiltagie <ramiltgv@gmail.com>
2025-08-21 16:15:15 +0400

    initial commit

---
+++ src/main/java/com/metabolicintelligence/MetabolicIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.metabolicintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetabolicIntelligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetabolicIntelligenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metabolicintelligence/MetabolicIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metabolicintelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetabolicIntelligenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
